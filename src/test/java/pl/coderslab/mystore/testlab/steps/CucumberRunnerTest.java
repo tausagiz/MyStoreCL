a8980ae03c9cd81d610977de74de1dd42e5d4aca
Mateusz Rusnak <mateusz@rusnak.biz>
2022-08-30 05:01:53 +0200

    Features path fixed.

--- src/test/java/pl/coderslab/mystore/testlab/steps/CucumberRunnerTest.java
+++ src/test/java/pl/coderslab/mystore/testlab/steps/CucumberRunnerTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/pl.coderslab.mystore.testlab/features/",
+        features = "src/test/resources/pl/coderslab/mystore/testlab/features/",
         glue = "pl.coderslab.mystore.testlab.steps")
 
 public class CucumberRunnerTest {
